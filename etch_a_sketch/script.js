a03a09b68f36807cea5dd84c5672dc6225455d4e
nisarg <nisargraval95@gmail.com>
2015-08-25 11:43:59 -0400

    Added functionality for increasing opacity with each hover

--- etch_a_sketch/script.js
+++ etch_a_sketch/script.js
@@ -17,7 +17,7 @@
       $(this).css({transition : 'background-color .001s ease-in-out',
                     'background-color':'yellow'});
     }, function() {
-      $(this).css({transition : 'background-color .3s ease-in-out',
+      $(this).css({transition : 'background-color 2s ease-in-out',
                     'background-color':'black'});
     });
 }
@@ -39,6 +39,27 @@ function init2() {
   $('.grid').on("mouseover", function() {
     $(this).css({transition: 'background-color .3s ease-in-out',
                   'background-color': 'yellow'});
+  });
+}
+
+function init3() {
+  var size=prompt("Enter grid size");
+  var width=($("#container").width()-250)/size;
+  var height=($("#container").height()-250)/size;
+  var margin=50/size;
+  var opacity=0.1;
+    for(i=1;i<=size;i++){
+      for(j=1;j<=size;j++){
+          $('<div class="grid"/div>').css({
+            "width": width, "height":height, "margin": margin, "opacity":opacity
+          }).appendTo("#container");
+        }
+      $("#container").append("<br/>");
+}
+
+  $('.grid').on("mouseover", function() {
+      opacity=($(this).css('opacity')*10+1)/10;
+      $(this).css({"opacity": opacity});
   });
 }
 
